c1101f21b35183c28cbd6af731b87189277f9ede
AmoPrecious <ap.lisene@gmail.com>
2023-06-15 09:09:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Programming puts the building
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

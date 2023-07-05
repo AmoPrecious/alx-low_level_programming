036518d5e77c93cd6e1eca39abf07c8eb6be6a8e
AmoPrecious <ap.lisene@gmail.com>
2023-07-05 07:02:36 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\n')
+if (*s)
 {
-_putchar('\n');
-return;
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 
-_putchar(*s);
-s++;
-_puts_recursion(s);
+else
+_putchar('\n');
 }
